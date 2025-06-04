afa039712137efb8ce700c853eb197e010d499bd
lucaskellem <lucaskellem13@gmail.com>
2025-06-03 21:22:40 -0600

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,2 @@
-<script lang="ts">
-
-</script>
-<h1>Under Construction!</h1>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			// default options are shown:
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: null,
+			fallback: undefined,
 			precompress: false,
-			strict: false
+			strict: true
 		})
 	}
 };
